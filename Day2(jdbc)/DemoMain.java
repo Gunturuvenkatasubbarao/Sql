0a996cf5539780114f4a3c0b182838f1dac47470
Gunturuvenkatasubbarao <gunturu.venkatasubbarao@ltts.com>
2021-03-08 14:50:59 +0530

    jdbc

---
+++ Day2(jdbc)/DemoMain.java
@@ -0,0 +1,29 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DemoMain {
+public static void main(String[] args)throws Exception {
+	Connection c =DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
+	Statement s=c.createStatement();
+	//s.execute("insert into customer values(2,'Gvs','6655443322')");
+	ResultSet rs=s.executeQuery("select*from customer");
+	while(rs.next()) {
+		System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+	}
+	s.execute("update cutomer Set cname='bala' where cno=2");
+	 System.out.println("after update");
+     ResultSet rs1=s.executeQuery("select * from customer" );
+       
+       while(rs1.next())
+       {
+       	System.out.println(rs1.getInt(1)+" "+rs1.getString(2)+" "+rs1.getString(3));
+       }
+	c.close();
+}
+}
+  
+
